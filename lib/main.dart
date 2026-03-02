837de132116d247c2ed26857361d7e114cba6caf
michalprog <michalch001@gmail.com>
2026-03-02 14:41:21 +0100

    start

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+void main() {
+  runApp(
+    const ProviderScope(
+      child: MyApp(),
+    ),
+  );
+}
+
+
+class MyApp extends ConsumerWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return const MaterialApp(
+      home: Placeholder()
+    );
+  }
+}
+
