bf96a3c48106d57a4857db35013a4196f9b7bb90
michalprog <michalch001@gmail.com>
2026-03-05 14:36:06 +0100

    repair error main33view

--- lib/33_section/window_widgets/game_33_page.dart
+++ lib/33_section/window_widgets/game_33_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class Game33Page extends StatelessWidget {
   const Game33Page({super.key, required this.child});
@@ -7,6 +8,17 @@ class Game33Page extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: const Text('Gra 33')), body: child);
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          onPressed: () => context.goNamed('home'),
+          icon: Icon(Icons.arrow_circle_left),
+        ),
+        backgroundColor: Colors.greenAccent,
+        centerTitle: true,
+        title: const Text('Gra 33'),
+      ),
+      body: child,
+    );
   }
 }
--- lib/33_section/window_widgets/main_33_view.dart
+++ lib/33_section/window_widgets/main_33_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:gierkownia2/33_section/widgets/action_button_33.dart';
+import 'package:go_router/go_router.dart';
 
 class Main33View extends StatelessWidget {
   const Main33View({super.key});
@@ -7,23 +8,26 @@ class Main33View extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ListView(
+      padding: const EdgeInsets.all(16),
       children: [
-        Row(
-          children: [
-            ActionButton33(title: "rozpocznij grę ",
-                secondLineTitle: "(1 vs 1 lokalnie) ",
-                onPressed: (){}),
-            ActionButton33(title: "rozpocznij grę ",
-                secondLineTitle: "(z botem) ",
-                onPressed: (){}),
-            ActionButton33(title: "rozpocznij grę ",
-                secondLineTitle: "(1 vs 1 sieć) ",
-                onPressed: (){}),
-
-          ],
+        ActionButton33(
+          title: 'rozpocznij grę ',
+          secondLineTitle: '(1 vs 1 lokalnie) ',
+          onPressed: () => context.goNamed('game-33'),
+        ),
+        const SizedBox(height: 12),
+        ActionButton33(
+          title: 'rozpocznij grę ',
+          secondLineTitle: '(z botem) ',
+          onPressed: () => context.goNamed('game-33'),
+        ),
+        const SizedBox(height: 12),
+        ActionButton33(
+          title: 'ustawienia ',
+          secondLineTitle: '(gra 33) ',
+          onPressed: () => context.goNamed('settings-33'),
         ),
       ],
-
     );
   }
 }
