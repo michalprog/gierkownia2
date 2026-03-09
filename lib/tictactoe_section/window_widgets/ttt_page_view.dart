94e820114e634fb6c1ffe0c8bb5115f4db6fae08
michalprog <michalch001@gmail.com>
2026-03-09 17:32:48 +0100

    setup ttt

--- lib/tictactoe_section/window_widgets/ttt_page_view.dart
+++ lib/tictactoe_section/window_widgets/ttt_page_view.dart
@@ -1,12 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class TttPageView extends StatelessWidget {
-  const TttPageView({super.key});
-
+  const TttPageView({super.key, required this.widget});
+final Widget widget;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        leading: IconButton(
+          onPressed: () => context.goNamed('home'),
+          icon: Icon(Icons.arrow_circle_left),
+        ),
+        backgroundColor: Colors.greenAccent,
+        centerTitle: true,
+        title: const Text('kółko i krzyżyk'),
+      ),
+      body: widget,
 
     );
   }
